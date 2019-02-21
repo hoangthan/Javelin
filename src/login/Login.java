33eb03957bebd5163a255a6a9814caffc6570a11
TommyThan <hoangthan345@gmail.com>
2019-02-21 13:51:31 +0700

    First commit

---
+++ src/login/Login.java
@@ -0,0 +1,23 @@
+package login;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Login extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
