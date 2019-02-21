4aba3270f59a7904db079c46d16a59022b96f378
TommyThan <hoangthan345@gmail.com>
2019-02-21 23:48:28 +0700

    Update

---
+++ src/Model/File.java
@@ -0,0 +1,43 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class File {
+
+    private String name;
+    private String owner;
+    private ArrayList<String> viewers;
+
+    public File() {
+    }
+
+    public File(String name, String owner, ArrayList<String> viewers) {
+        this.name = name;
+        this.owner = owner;
+        this.viewers = viewers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public ArrayList<String> getViewers() {
+        return viewers;
+    }
+
+    public void setViewers(ArrayList<String> viewers) {
+        this.viewers = viewers;
+    }
+}
