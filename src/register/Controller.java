0c10ecfb354d6f1b374dba0d36ea99eaebb062b8
TommyThan <hoangthan345@gmail.com>
2019-02-26 08:56:36 +0700

    Add controller

--- src/register/Controller.java
+++ src/register/Controller.java
@@ -1,9 +1,14 @@
 package register;
 
+import controller.Validate;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -12,6 +17,19 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     double x,y;
+
+    @FXML
+    Text lbNoti;
+    @FXML
+    TextField edtEmail;
+    @FXML
+    TextField edtPassword;
+    @FXML
+    TextField edtRePassword;
+    @FXML
+    ProgressBar pgbLogin;
+    @FXML
+    TextField edtCode;
 
     @FXML
     void mouseDraged(MouseEvent event) {
@@ -40,8 +58,36 @@ public class Controller implements Initializable {
         stage.close();
     }
 
+    @FXML
+    void register(MouseEvent event)
+    {
+        if(Validate.checkEmail(edtEmail.getText().toLowerCase().trim()))
+        {
+            if(edtPassword.getText().equals(edtRePassword.getText()))
+            {
+                if(Validate.checkPassword(edtPassword.getText()))
+                {
+                    if(edtCode.getText().trim()=="123"){
+                        lbNoti.setVisible(false);
+                        pgbLogin.setVisible(true);
+                    }else {
+                        lbNoti.setText("Active code is invalid.");
+                    }
+                }else {
+                    lbNoti.setText("Password is too weak.");
+                }
+            }else {
+                lbNoti.setText("Password is not match.");
+            }
+        }else {
+            lbNoti.setText("Email is not valid");
+            lbNoti.setVisible(true);
+            lbNoti.setStyle("-fx-text-inner-color: #0f9c58;");
+        }
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        //Initialize somethings
     }
 }
