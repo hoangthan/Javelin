1036ba7f81d8cdbc0459e05a4da141a662ac2642
TommyThan <hoangthan345@gmail.com>
2019-03-19 00:17:11 +0700

    Upload file functional

--- src/Home/Controller.java
+++ src/Home/Controller.java
@@ -10,9 +10,7 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.ContextMenuEvent;
@@ -25,12 +23,14 @@ import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import model.FileObject;
+import socketConnection.SendFile;
 import socketConnection.SendFileObject;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -46,7 +46,10 @@ public class Controller implements Initializable {
     BorderPane paneRoot;
     @FXML
     AnchorPane paneCenter;
+    @FXML
+    ProgressBar progressBar;
 
+    Node nodeTemp1;
     Image image;
     ImageView imageView;
     VBox vBox;
@@ -71,37 +74,34 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        nodeTemp1 = paneRoot.getCenter();
     }
-
-
 
     @FXML
     public void myDrive(MouseEvent mouseEvent){
+        progressBar.setProgress(ProgressIndicator.INDETERMINATE_PROGRESS);  //Running effect
         ArrayList<FileObject> list;
         FileObject fileObject = new FileObject();
         fileObject.setParent("root");
         list = SendFileObject.getResult(fileObject);
-
+        paneRoot.setCenter(nodeTemp1);
         paneContent.getChildren().clear();
 
+        Image image = new Image("images/docx.png");
         for(int i=0;i<100;i++){
-            image = new Image("images/docx.png");
-            imageView = new ImageView(image);
+
+            ImageView imageView = new ImageView(image);
             imageView.setFitWidth(60);
             imageView.setFitHeight(60);
-            text = new Text("Hello world");
-            vBox = new VBox();
+            Text text = new Text("Hello world");
+            VBox vBox = new VBox();
             vBox.getChildren().addAll(imageView,text);
             paneContent.getChildren().add(vBox);
-
-            vBox.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    vBox.setBackground(new Background(new BackgroundFill(Color.web("#b2bec3"), CornerRadii.EMPTY, Insets.EMPTY)));
-                }
+            vBox.setOnMouseClicked(e -> {
+                vBox.setBackground(new Background(new BackgroundFill(Color.web("#b2bec3"), CornerRadii.EMPTY, Insets.EMPTY)));
             });
         }
+        progressBar.setProgress(1);
     }
 
     @FXML
@@ -115,8 +115,8 @@ public class Controller implements Initializable {
                 Stage stage = (Stage) paneRoot.getScene().getWindow();
                 FileChooser fileChooser = new FileChooser();
                 fileChooser.setTitle("Pick your file you want to upload.");
-                File file =  fileChooser.showOpenDialog(stage);
-                System.out.println(file.getName());
+                List<File> list =  fileChooser.showOpenMultipleDialog(stage);
+                uploadFile(list);
             }
         });
 
@@ -134,12 +134,33 @@ public class Controller implements Initializable {
             @Override
             public void handle(DragEvent event) {
                 List<File> files = event.getDragboard().getFiles();
-                System.out.println(files.get(0).getName());
+                uploadFile(files);
             }
         });
 
     }
 
+    private void uploadFile(List<File> files){
+        progressBar.setProgress(ProgressIndicator.INDETERMINATE_PROGRESS);
+        boolean result = SendFile.send(files);
+        progressBar.setProgress(1);
+        showAlert(result);
+    }
 
+    private void showAlert(boolean result){
+        if(!result){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Upload fail");
+            alert.setHeaderText("Some thing went wrong. Try again.");
+            alert.setContentText("-Check your internet connection.\n-Check your account.\n-Server can be error.");
+            alert.show();
+        }else{
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Upload file success.");
+            alert.setHeaderText("Congratulations!!!");
+            alert.setContentText("Your file has been uploaded completely.");
+            alert.show();
+        }
+    }
 
 }
---
+++ src/socketConnection/SendFile.java
@@ -0,0 +1,57 @@
+package socketConnection;
+
+import fileHandler.TokenFile;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.List;
+
+public class SendFile {
+
+    private static String token = new TokenFile().getToken();
+
+    public static boolean send(List<File> files) {
+
+        try {
+            for(File file: files) System.out.println(file.getName() + ": " +file.getPath());
+            Socket socket = new Socket("localhost",2310);
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream =  new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeUTF(token);             //Send the token to authentication
+            boolean auth = dataInputStream.readBoolean(); //Get the authenticaction result.
+            if(!auth){
+                return false;                             //If token is not valid, notice for client.
+            }
+
+            dataOutputStream.writeInt(files.size()  );      //Send number of file to server.
+            for(File file: files){
+                dataOutputStream.writeUTF(file.getName());  //Send the name of file before
+                dataOutputStream.writeLong(file.length()); //Send the length of file
+                System.out.println("Size of file "+file.length());
+                sendFile(socket, file);           //Send the file
+            }
+            dataInputStream.close();
+            dataOutputStream.close();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    private static void sendFile(Socket socket, File file) throws IOException {
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        FileInputStream fis = new FileInputStream(file);
+        byte[] buffer = new byte[4096];
+
+        int read;
+        while ((read=fis.read(buffer)) > 0) {
+            dos.write(buffer,0,read);
+        }
+        fis.close();
+        dos.close();
+    }
+
+
+}
